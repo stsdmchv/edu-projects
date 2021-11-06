0b9861638caace676271362450dbbfded021a100
Stanislav Demichev <stsdmchv@gmail.com>
2021-11-06 23:10:44 +0300

    first-project: front done

--- project-1/src/app.js
+++ project-1/src/app.js
@@ -1,1 +1,33 @@
-console.log('entry point');
+import {isValid} from './utils';
+import './style.css'
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+const log = document.getElementById('log')
+
+function submitFormHandler(event) {
+
+  event.preventDefault()
+  if (isValid(input.value)){
+
+    const question = {
+      text:input.value.trim(),
+      date: new Date().toJSON(),
+    }
+    submitBtn.disabled = true
+
+    //Async request to server to save question
+    log.textContent = `Question: ${question.text}, ${question.date}`
+    input.value=''
+
+    input.className=''
+    submitBtn.disabled = false
+  }
+}
+
+input.addEventListener('input', ()=>{
+  submitBtn.disabled = !isValid(input.value)
+})
+
+form.addEventListener('submit', submitFormHandler) //press button event
--- project-1/webpack.config.js
+++ project-1/webpack.config.js
@@ -15,5 +15,13 @@ module.exports = {
     new HTMLPlugin({
       template: './src/index.html'
     }),new CleanWebpackPlugin()
-  ]
+  ],
+  module:{
+    rules: [
+      {
+        test: /\.css$/i,
+        use:['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
